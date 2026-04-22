bd28df2770008204cd477b23c129f0d0f237cd55
Iirne <emil@caerli.dk>
2026-04-22 13:28:06 +0200

    sorting

--- src/main/java/portfolioOpgave/Sort/Sort.java
+++ src/main/java/portfolioOpgave/Sort/Sort.java
@@ -1,18 +1,37 @@
 package portfolioOpgave.Sort;
 
-import javax.print.DocFlavor;
 import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int n = 100;
+        int n = 10000000;
         int[] array = getRandomArray(n);
+/*
+        System.out.println("Bubblesort");
+        long start = System.currentTimeMillis();
+        bubbleSort(array);
+        System.out.println("ms: " + (System.currentTimeMillis() - start));
+        System.out.println("is array sorted:" + isSorted(array));
+        System.out.println();
 
-        //Arrays.stream(bubbleSort(array)).forEach(System.out::println);
 
-        //array = getRandomArray(n);
+*/
+        array = getRandomArray(n);
 
-        Arrays.stream(mergeSort(array)).forEach(System.out::println);
+        System.out.println("Mergesort");
+        long start = System.currentTimeMillis();
+        mergeSort(array);
+        System.out.println("ms: " + (System.currentTimeMillis() - start));
+        System.out.println("is array sorted:" + isSorted(array));
+        System.out.println();
+        array = getRandomArray(n);
+
+        System.out.println("Quicksort");
+        start = System.currentTimeMillis();
+        quicksort(array,0,array.length-1);
+        System.out.println("ms: " + (System.currentTimeMillis() - start));
+        System.out.println("is array sorted:" + isSorted(array));
+        System.out.println();
 
     }
 
@@ -33,20 +52,19 @@ public class Sort {
         if(array.length<2){
             return array;
         }
-
         //splits the array in 2 halves
-        //in cases of unequal the left gets more
-        //[0, 1], [2]
-        int[] leftHalf = Arrays.copyOfRange(array,0,(int)(array.length/2));
-        int[] rightHalf = Arrays.copyOfRange(array,(int)(array.length/2),array.length);
+        //in cases of unequal lengths the right gets more
+        //[0], [1, 2]
+        int[] leftHalf = Arrays.copyOfRange(array,0,(array.length/2));
+        int[] rightHalf = Arrays.copyOfRange(array,(array.length/2),array.length);
 
-        leftHalf = mergeSort(leftHalf);
-        rightHalf = mergeSort(rightHalf);
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
 
         int l = 0, r=0;
-
-        for(int i = 0; i<array.length; i++){
-            //&& r >= rightHalf.length  ) || leftHalf[l]<rightHalf[r]
+        //if the right side  is counted only add left
+        //if the left side isn't counted and the left half is smaller than the right half
+        for(int i = 0; i < array.length; i++){
             if(rightHalf.length <= r || (l < leftHalf.length && leftHalf[l]<rightHalf[r])){
                 array[i] = leftHalf[l];
                 l++;
@@ -59,7 +77,31 @@ public class Sort {
         return array;
     }
 
+    private static int[] quicksort(int[] array, int low, int high){
+        if (low >= high){
+            return array;
+        }
 
+        int i = low, pivot = array[high];
+        for(int j = low; j < high; j++){
+            if(array[j]<pivot){
+                Swap(array,i,j);
+                i++;
+            }
+        }
+        Swap(array,i,high);
+
+        quicksort(array,low, i-1);
+        quicksort(array, i+1, high);
+
+        return array;
+    }
+
+    private static void Swap(int[] array,int i, int j){
+        int carrier = array[j];
+        array[j] = array[i];
+        array[i] = carrier;
+    }
 
     private static int[] getRandomArray(int n){
         int[] result = new int[n];
@@ -67,5 +109,14 @@ public class Sort {
             result[i] = (int) (Math.random() * n);
         }
         return result;
+    }
+
+    private static boolean isSorted(int[] toCheck){
+        for(int i = 1; i < toCheck.length; i++){
+            if (toCheck[i] < toCheck[i-1]){
+                return false;
+            }
+        }
+        return true;
     }
 }
