57c07ae4e506844608ff985c0b24b5b0a8aca755
Iirne <emil@caerli.dk>
2026-05-14 22:52:28 +0200

    made an A* algorithm

--- src/main/java/portfolioOpgave/graphs/Astar/AStar.java
+++ src/main/java/portfolioOpgave/graphs/Astar/AStar.java
@@ -27,19 +27,15 @@ public class AStar {
 
                 switch (grid[r][c]){
                     case "1":
-                        System.out.println("1");
                         nodes[r][c] = new CityNode(r, c, "vej", 2);
                         break;
                     case "2":
-                        System.out.println("2");
                         nodes[r][c] = new CityNode(r, c, "Motorvej",  1);
                         break;
                     case "3":
-                        System.out.println("3");
                         nodes[r][c] = new CityNode(r, c, "snejlvej", 4);
                         break;
                     default:
-                        System.out.println("default");
                         nodes[r][c] = new CityNode(r, c, grid[r][c], 3);
                         break;
                 }
@@ -64,44 +60,51 @@ public class AStar {
 
         CityNode End = nodes[5][6];
 
-        findShortestPath(start, End, 0);
 
+
+        //findShortestPath(start, End, 0);
         findShortestPath(start, End, 1);
+        //findShortestPath(start, End, 1.5f);
 
-        findShortestPath(start, End, 3);
+
     }
 
     private static void findShortestPath(CityNode start, CityNode end, float heuristicMultiplier){
         HashMap<CityNode, PathCarrier> prev = new HashMap<>();
         HashSet<CityNode> visited = new HashSet<>();
         PriorityQueue<PathCarrier> queue = new PriorityQueue<>();
-        queue.add(new PathCarrier(0,start));
-        prev.put(start,new PathCarrier(0,null));
+        queue.add(new PathCarrier(heuristicMultiplier * heuristic(start,end),start));
+        prev.put(start,new PathCarrier(heuristicMultiplier * heuristic(start,end),null));
 
         while (!queue.isEmpty()){
             PathCarrier current = queue.poll();
             if (visited.contains(current.node)) break;
-            if (current.node == end) break;
+            if (current.node == end) {
+                break;
+            }
             visited.add(current.node);
-
 
             for (CityNode node: current.node.getNeighbors()){
                 int distance = node.getWeight();
                 if (visited.contains(node)) continue;
 
                 float newDist = distance + current.pathlength;
-                double heuristic = Math.sqrt(current.node.getCol()*current.node.getCol() + current.node.getRow()*current.node.getRow()); // squared distance
+                float heuristic = heuristicMultiplier * heuristic(node, end); // squared distance
 
-                if (newDist < prev.getOrDefault(node, new PathCarrier(Integer.MAX_VALUE,null)).pathlength){
-                    prev.put(node, new PathCarrier(newDist + (float) (heuristicMultiplier * heuristic), current.node));
-                    queue.add(new PathCarrier(newDist, node));
+                float sum = newDist + heuristic;
+
+                if (sum < prev.getOrDefault(node, new PathCarrier(Integer.MAX_VALUE,null)).pathlength){
+                    prev.put(node, new PathCarrier(sum, current.node));
+                    queue.add(new PathCarrier(sum, node));
                 }
             }
         }
+
         List<String> path = new ArrayList<>();
         String[][] grid = new String[ROWS][COLS];
         CityNode current = end;
-        while(prev.get(current).node != null){
+
+        while(prev.get(current) != null && prev.get(current).node != null){
             PathCarrier node = prev.get(current);
             grid[node.node.getRow()][node.node.getCol()] = node.node.getName();
             path.add(node.node.getName());
@@ -113,8 +116,11 @@ public class AStar {
         for (String[] row : grid){
             System.out.println(Arrays.toString(row));
         }
-        System.out.println(Arrays.deepToString(grid));
         System.out.println("Samlet dist: " + prev.get(end).pathlength);
+    }
+
+    private static float heuristic(CityNode placement, CityNode goal){
+        return (float) Math.sqrt(Math.pow(goal.getCol() - placement.getCol(),2) + Math.pow(goal.getRow() - placement.getRow(),2));
     }
 
     private static class PathCarrier implements Comparable<PathCarrier>{
